f558ab5b52f657d2b9d9b0a2f9beddff80d8a71d
Martin Ondrejka <martin.ondrejka@gmail.com>
2026-01-21 16:07:47 +0100

    added GlobalExceptionHandler

---
+++ src/main/java/eu/martin/store/common/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package eu.martin.store.common;
+
+import eu.martin.store.cart.ItemNotFoundException;
+import eu.martin.store.cart.QuantityExceedException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException exception) {
+        var errors = new HashMap<String, String>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    ResponseEntity<String> handleEntityNotFound(EntityNotFoundException ex) {
+        return ResponseEntity.status(NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(QuantityExceedException.class)
+    ResponseEntity<String> handleEntityNotFound(QuantityExceedException ex) {
+        return ResponseEntity.status(BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ItemNotFoundException.class)
+    ResponseEntity<String> handleEntityNotFound(ItemNotFoundException ex) {
+        return ResponseEntity.status(BAD_REQUEST).body(ex.getMessage());
+    }
+}
