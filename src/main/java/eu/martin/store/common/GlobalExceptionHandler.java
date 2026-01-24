14f5f6af6af7130d176ea102f47a92d013fbf7b0
Martin Ondrejka <martin.ondrejka@gmail.com>
2026-01-24 10:23:58 +0100

    updated GlobalExceptionHandler

--- src/main/java/eu/martin/store/common/GlobalExceptionHandler.java
+++ src/main/java/eu/martin/store/common/GlobalExceptionHandler.java
@@ -6,7 +6,9 @@ import eu.martin.store.users.DuplicateUserException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,6 +21,42 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+    /*@ExceptionHandler(HandlerMethodValidationException.class)
+    ResponseEntity<Map<String, String>> handleHandlerMethodValidation2(HandlerMethodValidationException exception) {
+        var errors = new HashMap<String, String>();
+
+        for (var validationResult : exception.getParameterValidationResults()) {
+            validationResult.getResolvableErrors().forEach(messageSourceResolvable -> System.out.println("arguments: " + Arrays.toString(messageSourceResolvable.getArguments())));
+            validationResult.getResolvableErrors().forEach(messageSourceResolvable -> System.out.println("default message: " + messageSourceResolvable.getDefaultMessage()));
+            System.out.println("------");
+        }
+
+
+        return ResponseEntity.badRequest().body(errors);
+    }*/
+/*
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ProblemDetail handleMethodValidationException2(HandlerMethodValidationException ex) {
+        // Create the Problem Detail object
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_REQUEST,
+                "Validation Failed"
+        );
+
+        problemDetail.setType(URI.create("about:blank"));
+        problemDetail.setTitle("Method Argument Validation Failed");
+
+        // Extract errors
+        var errors = ex.getParameterValidationResults().stream()
+                .flatMap(res -> res.getResolvableErrors().stream()
+                        .map(err -> res.getMethodParameter().getParameterName() + " " + err.getDefaultMessage()))
+                .toList();
+
+        problemDetail.setProperty("errors", errors);
+
+        return problemDetail;
+    }
+*/
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException exception) {
         var errors = new HashMap<String, String>();
@@ -49,7 +87,17 @@ class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<Void> handleAccessDenied() {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    public ResponseEntity<String> handleAccessDenied(AccessDeniedException exception) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentials(BadCredentialsException exception) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(BAD_REQUEST).body(ex.getMessage());
     }
 }
