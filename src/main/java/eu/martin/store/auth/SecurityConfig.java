f6aa039b570c31f749f306c513deb48907a70d65
Martin Ondrejka <martin.ondrejka@gmail.com>
2026-01-29 14:27:08 +0100

    added authorization for openAPI(swagger)

--- src/main/java/eu/martin/store/auth/SecurityConfig.java
+++ src/main/java/eu/martin/store/auth/SecurityConfig.java
@@ -1,5 +1,9 @@
 package eu.martin.store.auth;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -62,5 +66,22 @@ class SecurityConfig {
                     c.accessDeniedHandler(((request, response, accessDeniedException) -> response.setStatus(HttpStatus.FORBIDDEN.value())));
                 })
                 .build();
+    }
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components().addSecuritySchemes(
+                                securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
