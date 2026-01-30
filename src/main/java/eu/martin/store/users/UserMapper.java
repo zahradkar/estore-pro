6bc3affe9d9d8cb666f1ed10a108aeee3910e436
Martin Ondrejka <martin.ondrejka@gmail.com>
2026-01-30 07:20:11 +0100

    added ignore properties to UserMapper in order to avoid gradle warnings

--- src/main/java/eu/martin/store/users/UserMapper.java
+++ src/main/java/eu/martin/store/users/UserMapper.java
@@ -7,12 +7,19 @@ import org.mapstruct.MappingTarget;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     @Mapping(constant = "USER", target = "role")
+    @Mapping(ignore = true, target = "verified")
+    @Mapping(ignore = true, target = "addresses")
     User toUserEntity(UserRequest dto);
 
     UserResponse toUserResponse(User user);
 
+    @Mapping(ignore = true, target = "verified")
+    @Mapping(ignore = true, target = "addresses")
+    @Mapping(ignore = true, target = "role")
     void update(UserRequest dto, @MappingTarget User user);
 
+    @Mapping(ignore = true, target = "loyaltyPoints")
+    @Mapping(ignore = true, target = "user")
     void update(ProfileRequest dto, @MappingTarget Profile profile);
 
     ProfileResponse toProfileResponse(Profile profile);
